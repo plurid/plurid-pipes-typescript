27d7362351e687b3acaea0c7da74c42e89590d01
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-01-05 14:25:08 +0200

    replacer > dev: source

--- packages/replacer/source/pipe/index.ts
+++ packages/replacer/source/pipe/index.ts
@@ -1,6 +1,28 @@
-// #region module
-const replacer = async () => {
+// #region imports
+    import {
+        promises as fs,
+    } from 'fs';
+// #endregion imports
 
+
+
+// #region module
+const replacer = async (
+    search: string,
+    replace: string,
+) => {
+    return (
+        file: string,
+    ) => {
+        try {
+
+            return true;
+        } catch (error) {
+            console.log('Replacer Pipe Error ::', error);
+
+            return false;
+        }
+    }
 }
 // #endregion module
 
