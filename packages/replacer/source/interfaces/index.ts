74806e2b079db9c0409d0937ce2950201dbd7e27
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-01-05 15:53:29 +0200

    replacer > dev: interfaces

--- packages/replacer/source/interfaces/index.ts
+++ packages/replacer/source/interfaces/index.ts
@@ -1,10 +1,20 @@
 // #region module
 export interface ReplacerOptions {
+    /**
+     * Regular Expression replacement flags.
+     *
+     * Default: `'gm'`.
+     */
     flags: string;
 }
 
 
 export interface ReplaceOptions {
+    /**
+     * Return the replaced result.
+     *
+     * Default: `false`.
+     */
     extract: boolean;
 }
 // #endregion module
